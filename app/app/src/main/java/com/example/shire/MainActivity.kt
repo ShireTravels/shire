4d3b48474511fc1d063d18f06f5ec96242b2d5c0
cribiking <Cribiking@gmail.com>
2026-02-21 18:58:43 +0100

    Feature: CREATED first layout HomeScreen() at 'ui' directory, MODIFIED: androidManifest.xml , added instruccions on how execute the app in 'activity' tag

--- app/app/src/main/java/com/example/shire/MainActivity.kt
+++ app/app/src/main/java/com/example/shire/MainActivity.kt
@@ -11,37 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.shire.ui.HomeScreen
 import com.example.shire.ui.theme.ShireTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ShireTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            HomeScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShireTheme {
-        Greeting("Android")
     }
 }
