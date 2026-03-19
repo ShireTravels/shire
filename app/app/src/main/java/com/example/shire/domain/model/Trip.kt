91b9cfe444ebd87d588ddab1a565f3f8cf2bc69d
cribiking <Cribiking@gmail.com>
2026-03-19 14:21:05 +0100

    feature: class APP to use hilt, activate hilt in MainActivity, app class registered in androidmanifest (i forgot to add changes)

--- app/app/src/main/java/com/example/shire/domain/model/Trip.kt
+++ app/app/src/main/java/com/example/shire/domain/model/Trip.kt
@@ -3,12 +3,14 @@ package com.example.shire.domain.model
 import java.util.LinkedList
 
 data class Trip(
-    var id: Int = 0,
+    var id: Int = 0, //ide del trip
+    var title: String,
+    var dates: String,
+    var price: Double,
     var hotel: HashMap<Int, Int>, //num dia / id hotel
-    var vuelo: HashMap<Int, Int>,
-    var coche: HashMap<Int, Int>,
+    var flight: HashMap<Int, Int>,
+    var car: HashMap<Int, Int>,
     var places: HashMap<Int, Int>,
-    var presupuesto: Double,
     var gallery: LinkedList<String>,
-    var notas: String
+    var notes: String
 )
